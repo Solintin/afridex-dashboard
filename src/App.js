acd35547b508e9eb1f67f95dbe4b91267aa96352
DevSaalih <soliualley@gmail.com>
2021-12-14 05:27:09 +0100

    Afridex Dashboard Init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 function App() {
   return (
     <div>
-      <h1>
-      Alley</h1>
+      <h1 className="text-3xl text-center text-red-500 font-bold underline">
+        Afridex Personal Dashboard
+      </h1>
     </div>
-  )
+  );
 }
 export default App;
